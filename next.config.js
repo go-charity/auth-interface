f8c45f5256252fbe6fc26eed473222e4d4a36519
onukwilip <onukwilip@gmail.com>
2023-09-21 19:41:04 +0100

    Reverted the redirect functionality changes

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ const nextConfig = {
         destination: "/login",
         permanent: true,
       },
-      {
-        source: "/api/:path*",
-        // destination: "https://auth-server.gocharity.com.ng/:path*",
-        destination: process.env.AUTH_BACKEND_HOST,
-        permanent: false,
-      },
     ];
   },
 };
