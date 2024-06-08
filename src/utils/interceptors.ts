ea58309b950f24645c7d631661fc336db57a9787
onukwilip <onukwilip@gmail.com>
2024-06-08 09:57:14 +0100

    Logged the OTP cookie values

--- src/utils/interceptors.ts
+++ src/utils/interceptors.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getCookie, setCookie } from "cookies-next";
+import { getCookie, getCookies, setCookie } from "cookies-next";
 
 export const authBackendInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_AUTH_BACKEND_HOST,
@@ -8,9 +8,11 @@ export const authBackendInstance = axios.create({
 });
 
 authBackendInstance.interceptors.request.use(
-  (config) => {
+  async (config) => {
     const access_token = getCookie("otp_access_token");
     const refresh_token = getCookie("otp_refresh_token");
+
+    console.log("cookies", getCookies());
 
     console.log("access token", access_token);
     console.log("refresh token", refresh_token);
